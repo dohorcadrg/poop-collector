eee9678f561db288af05e50cc8543cfd9501c079
dohorcadrg <glorylegaspi@hotmail.com>
2018-07-12 17:26:29 +1000

    Inital

---
+++ assets/images/grass-tiles.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass-sheet" tilewidth="100" tileheight="100" tilecount="36" columns="6">
+ <properties>
+  <property name="collides" type="bool" value="true"/>
+ </properties>
+ <image source="grass-sheet.png" width="600" height="600"/>
+</tileset>
---
+++ assets/tiles/grass-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass-sheet" tilewidth="100" tileheight="100" tilecount="25" columns="5">
+ <image source="../images/grass-sheet.png" width="500" height="500"/>
+</tileset>
